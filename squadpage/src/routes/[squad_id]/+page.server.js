14c4916319089495c26d5115f6dc1d8de3eada7a
zoepje <zoe.penner@hva.nl>
2024-09-05 18:43:56 +0200

    data fetch ook squads

--- squadpage/src/routes/[squad_id]/+page.server.js
+++ squadpage/src/routes/[squad_id]/+page.server.js
@@ -1,12 +1,15 @@
 import fetchJson from "$lib/fetch-json"
 
 export async function load({params}) {
-  console.log('PARAMS', params)
+
   const url = `https://fdnd.directus.app/items/person/?filter={"squad_id":${params.squad_id}}`
+  const squadurl = `https://fdnd.directus.app/items/squad/${params.squad_id}`
 
   const persons = await fetchJson(url)
+  const squad = await fetchJson(squadurl)
 
   return {
-    persons: persons.data
+    persons: persons.data,
+    squad: squad.data
   }
 }
