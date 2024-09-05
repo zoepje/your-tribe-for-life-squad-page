f6b4a9be141ffc1af37dffef836941f9ea0ec842
lisavanmansom <lisavanmansom@gmail.com>
2024-09-05 11:08:28 +0200

    page setup html
    2nd page test html added

--- squadpage/src/routes/+page.svelte
+++ squadpage/src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let data
+  
+    function checkAvatarImage (avatar) {
+      if (avatar.includes('https')) {
+        return true
+      } 
+  
+      return false
+    }
+  </script>
+  
+  <main>
+    <h1>Team 27</h1>
+    <p>Welcome to this squadpage, here you can find the two squads from year 2 of FDND</p>
+  
+    
+  </main>
+  
+  <style>
+
+    ul {
+      list-style: none;
+      padding:0;
+      display:flex;
+      gap:.5rem;
+      align-items:start;
+    }
+
+  </style>
