81a30226f8f91ee4777685701d656609e9c19114
zoepje <zoe.penner@hva.nl>
2024-09-05 18:43:21 +0200

    opmaak

--- squadpage/src/routes/+layout.svelte
+++ squadpage/src/routes/+layout.svelte
@@ -2,14 +2,64 @@
   export let data
 </script>
 
-<h1>Squadpage</h1>
+<h1><a href="/">Team<span>.27</span></a></h1>
 
-<ul>
-  {#each data.squads as squad}
-    <li>
-      <a href="/{squad.id}">{squad.name}</a>
-    </li>
-  {/each}
-</ul>
+<figure>
+  <figcaption>Classes</figcaption>
+  <ul>
+    {#each data.squads as squad}
+      <li>
+        <a href="/{squad.id}" class="nav-link">{squad.name}</a>
+      </li>
+    {/each}
+  </ul>
+</figure>
 
 <slot></slot>
+
+<style>
+  h1 {
+    font-family: Griffiths;
+    font-size: 4rem;
+    text-align: center;
+    font-weight: 300;
+
+    & span {
+      font-family: Poppins;
+      font-weight: bold;
+      font-size: 3rem;
+    }
+  }
+
+  figure{
+    margin: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+    & figcaption {
+      font-size: 1.5rem;
+      text-transform: uppercase;
+    }
+  }
+
+  ul {
+    padding: 0;
+    list-style: none;
+    display: flex;
+    justify-content: flex-end;
+
+    & li {
+      margin: 5px;
+    }
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  a.nav-link:hover{
+    text-decoration: underline wavy;
+    color: yellow;
+  }
+</style>
