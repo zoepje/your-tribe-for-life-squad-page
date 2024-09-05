a1553982d71503229cb5aea5ef544325454aa4fb
zoepje <zoe.penner@hva.nl>
2024-09-05 18:44:17 +0200

    styling en git links

--- squadpage/src/routes/[squad_id]/+page.svelte
+++ squadpage/src/routes/[squad_id]/+page.svelte
@@ -1,13 +1,50 @@
 <script>
   export let data
+
+  function checkGithubHandle (github) {
+    if (github.includes('https')) {
+      return true
+    } 
+
+    return false
+  }
 </script>
 
-<ul>
-  {#each data.persons as person}
-    {#if person.name.length > 0}
-      <li>
-        {person.name} {person.prefix} {person.surname}
-      </li>
-    {/if}
-  {/each}
-</ul>
+<main>
+  <h2>{data.squad.name}</h2>
+
+  <ul>
+    {#each data.persons as person}
+      {#if person.name.length > 0}
+        <li>
+          {person.name} {person.prefix} {person.surname}
+          <div class="contact-link">
+            {#if person.github_handle.length > 0 && checkGithubHandle (person.github_handle)}
+              <a href="{person.github_handle}">get in contact with {person.name}, Github</a>
+            {:else if person.github_handle.length > 0 || checkGithubHandle (person.github_handle)}
+              <a href="https://github.com/{person.github_handle}">get in contact with {person.name}, Github</a>  
+            {/if}  
+          </div>
+        </li>
+      {/if}
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main{
+    margin: 0 auto;
+    min-width: 250px;
+    width: 28vw;
+  }
+
+  h2{
+    margin: 1rem;
+    font-size: 2rem;
+    text-align: center;
+  }
+
+  ul {
+    list-style: none;
+  }
+</style>
