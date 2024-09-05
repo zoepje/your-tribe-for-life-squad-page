c2da42d3a6de3412f55f118fdb927da9020876a1
lisavanmansom <lisavanmansom@gmail.com>
2024-09-05 22:19:19 +0200

    directus connection w error

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import fetchJson from "$lib/fetch-json"
 
 export async function load() {
-  const url = 'https://fdnd.directus.app/items/person/?filter={"squad_id":3}'
+  const url = 'https://fdnd.directus.app/items/person/?filter={"squad_id":${params.squad_id}}'
 
   const person = await fetchJson(url)
 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,16 @@
-<script>
+p<script>
   export let data
 </script>
   
   <main>
     <h1>Team <span>27</span></h1>
     <p>Welcome to this squadpage, here you can find the two squads from year 2 of FDND</p>
-    <p>{data.person.name}</p>
-    <a href="./about/+page.svelte">link die niet werkt</a>
+    
+    <ul>
+      {#each data.person as person}
+        <li><p>{person.name}</p></li>
+      {/each}
+    </ul>
 
   
   </main>
@@ -26,9 +30,6 @@
 
     p {       text-align: center; max-width: 17em; }
 
-    a { color: white; text-decoration: none; cursor: pointer; 
-        margin: 5em 0; background-color: palevioletred; text-align: center; 
-        width: 12.5em; height: 3em; padding: .6em; border-radius: 5em; border: white .2em solid;}
-    
+   
   </style>
   
