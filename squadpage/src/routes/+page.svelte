b4e94820c776b81e7cfe330f108f16d1d6effb0d
zoepje <zoe.penner@hva.nl>
2024-09-05 18:43:39 +0200

    home page

--- squadpage/src/routes/+page.svelte
+++ squadpage/src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h2>Home screen</h2>
+<p>Welcome to this squadpage, here you can find the two squads from year 2 of FDND</p>
+
+<style>
+  p{
+    text-align: center;
+    font-size: 1.2rem;
+  }
+</style>
