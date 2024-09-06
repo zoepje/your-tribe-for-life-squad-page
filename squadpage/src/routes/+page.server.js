0f26c8ad28430d88eb7804524f5bb4574010b56e
lemuelmgsn <magsinolemuel68@gmail.com>
2024-09-06 14:35:47 +0200

    v1
    klein beginnetje

---
+++ squadpage/src/routes/+page.server.js
@@ -0,0 +1,11 @@
+import fetchJson from "$lib/fetch-json"
+
+export async function load() {
+  const url = "https://fdnd.directus.app/items/person/"
+
+  const persons = await fetchJson(url)
+
+  return {
+    persons: persons.data
+  }
+}
--- squadpage/src/routes/+page.svelte
+++ squadpage/src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  export let data
+
+  function checkAvatarImage (avatar) {
+    if (avatar.includes('https')) {
+      return true
+    } 
+
+    return false
+  }
+</script>
+
+<main>
+<body>
+
+  <h1>welcome to this squadpage, here you can find the two squads from year 2 of FDND</h1>
+
+  <ul>
+    {#each data.persons as person}
+
+      <li>
+       
+
+        <p>{person.name}</p>
+        <p>{person.surname}</p>
+        
+      </li>
+
+    {/each}
+  </ul>
+
+</body>
+</main>
+
+<style>
+
+  body{
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    background-color: #252422;
+  }
+
+  ul {
+    list-style: none;
+    padding:0;
+    display:flex;
+    flex-direction: column;
+    gap: 5px;
+    align-items:start;
+  }
+  p{
+    color:white;
+  }
+  
+
+</style>
