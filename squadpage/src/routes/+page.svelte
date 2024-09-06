379accfbb4ed692ec1a13ccf159c0a0bfca88285
MisahSaid <misah.said.said@hva.nl>
2024-09-06 12:39:57 +0200

    set up van pagina

--- squadpage/src/routes/+page.svelte
+++ squadpage/src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  export let data
+</script>
+
+<header>
+    <img src="./static/newlogo.svg">
+    <a href="#">TEAM27</a>
+</header>
+
+<main>
+    <!-- top of the page -->
+    <h1>WELCOME TO THIS SQUADPAGE, HERE YOU CAN FIND THE TWO CLASSES FROM YEAR 2 FDND</h1>
+    <button>SCROLL</button>
+
+    <!-- second half of the page [name list] -->
+    <ul>
+        {#each data.persons as person}
+            {#if person.name.length > 0}
+            <li>
+                {person.name} {person.prefix} {person.surname}
+            </li>
+            {/if}
+        {/each}
+    </ul>
+</main>
+
+
+<style>
+    li{
+        list-style-type: none;
+    }
+    
+    h1{
+        font-size: 42px;
+        text-align: center ;
+        color: rgba(255, 255, 255, 0.6);
+    }
+</style>
